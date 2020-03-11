8d119a899c965bbccef7e0d4228b71a32eea3ac8
michelle-hey <miscmiche@gmail.com>
2020-03-10 20:20:08 -0400

    Styling added, return later for improved styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import Apod from "./Components/DailyImg/Apod";
+import Title from "./Components/Text/TextTitle";
+import DailyImage from "./Components/APOD/DailyImage";
 import Text from "./Components/Text/text";
+
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
 
 function App() {
   const [data, setData] = useState([]);
   const [text, setText] = useState([]);
+  const [title, setTitle] = useState([]);
 
   useEffect(() => {
       axios
@@ -16,15 +21,59 @@ function App() {
                   // console.log(res.data.explanation);
                   setData(res.data.url);
                   setText(res.data.explanation);
+                  setTitle(res.data.title);
                 })
               .catch( err => {console.log(err)} )
   }, [])
   
+  const ContainerDiv = styled.div`
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      flex-wrap: wrap;
+      margin: 100px;
+      width: 100%;
+      border-radius: 37px;
+      background-color: #CCCFCF;
+      box-shadow:  6px 6px 8px #e8ded3, 
+                    -6px -6px 8px #e8ded3;
+  `;
+
+  const LeftDiv = styled.div`
+      flex-direction: row;
+      justify-content: space-around;
+      flex-wrap: wrap;
+      width: 30%;
+      border-radius: 37px;
+      background-color: #CCCFCF;
+      box-shadow:  6px 6px 8px #e8ded3, 
+                    -6px -6px 8px #e8ded3;
+  `;
+  const RightDiv = styled.div`
+      flex-direction: row;
+      justify-content: center;
+      align-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      width: 65%;
+      padding: 2%;
+      border-radius: 37px;
+      background-color: #CCCFCF;
+      box-shadow:  6px 6px 8px #e8ded3, 
+                    -6px -6px 8px #e8ded3;
+  `;
  
   return (
     <div className="App">
-      <Apod url={data}/>
-      <Text text={text}/>
+      <ContainerDiv>
+          <LeftDiv className="leftDiv">
+            <Title title={title}/>
+            <Text text={text}/>
+          </LeftDiv>
+          <RightDiv className="rightDiv">
+            <DailyImage url={data}/>
+          </RightDiv>
+      </ContainerDiv>
     </div>
   );
 }
